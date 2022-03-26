cfcae842c7f25200b32463488d612e8b9de1f3e1
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2022-03-26 12:01:11 +0200

    ✨ Adds Craft

---
+++ Craft/Craft.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using MoreMountains.InventoryEngine;
+using UnityEngine;
+
+namespace Craft
+{
+    [Serializable]
+    public abstract class DisplayNiceNameInInspectorList : ISerializationCallbackReceiver
+    {
+        [HideInInspector]
+        [SerializeField]
+        protected string _name;
+
+        protected abstract void SetName();
+
+        public void OnBeforeSerialize()
+        {
+            SetName();
+        }
+ 
+        public void OnAfterDeserialize()
+        {
+            SetName();
+        }
+    }
+    
+    [Serializable]
+    public class Ingredient : DisplayNiceNameInInspectorList
+    {
+        public InventoryItem Item;
+        public int Quantity;
+
+        protected override void SetName()
+        {
+            _name = Quantity + " x " + (Item == null ? "null" : Item.ItemName);
+        }
+
+        public override string ToString()
+        {
+            return Quantity + " " + Item.ItemName + (Quantity > 1 ? "s" : "");
+        }
+    }
+
+    [Serializable]
+    public class Recipe : DisplayNiceNameInInspectorList
+    {
+        public Ingredient[] Ingredients;
+        public InventoryItem Result;
+        public string IngredientsText => string.Join(", ", Ingredients.Select(ingredient => ingredient.ToString()));
+     
+        protected override void SetName()
+        {
+            _name = Result == null ? "null" : Result.ItemName;
+        }
+    }
+
+    public static class Crafting
+    {
+        public static bool CanCraft(this Inventory inventory, Recipe recipe)
+        {
+            return !recipe.Ingredients.Any(ingredient => inventory.InventoryContains(ingredient.Item.ItemID).Sum(index => inventory.Content[index].Quantity) < ingredient.Quantity);
+        }
+        
+        public static void Craft(this Inventory inventory, Recipe recipe)
+        {
+            if (!inventory.CanCraft(recipe)) return;
+            foreach (var ingredient in recipe.Ingredients)
+                inventory.RemoveItemByID(ingredient.Item.ItemID, ingredient.Quantity);
+            inventory.AddItem(recipe.Result, 1);
+        }
+    }
+    
+    [CreateAssetMenu]
+    public class Craft : ScriptableObject
+    {
+        public Recipe[] Recipes;
+    }
+}
---
+++ Craft/CraftingButtons.cs
@@ -0,0 +1,34 @@
+﻿using MoreMountains.InventoryEngine;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Craft
+{
+    public class CraftingButtons : MonoBehaviour
+    {
+        [Tooltip("The inventory where the ingredients for crafting are found and where the resulting item will be added")]
+        public Inventory CraftingInventory;
+        [SerializeField]
+        private Craft Craft;
+        private GameObject _craftingButton;
+
+        private void Awake()
+        {
+            _craftingButton = transform.GetChild(0).gameObject;
+        }
+
+        private void Start()
+        {
+            foreach (var recipe in Craft.Recipes)
+            {
+                var craftingButton = Instantiate(_craftingButton, transform);
+                craftingButton.transform.GetChild(0).GetComponent<Text>().text = recipe.Result.ItemName;
+                craftingButton.transform.GetChild(1).GetComponent<Text>().text = recipe.Result.ShortDescription;
+                craftingButton.transform.GetChild(2).GetComponent<Image>().sprite = recipe.Result.Icon;
+                craftingButton.transform.GetChild(3).GetComponent<Text>().text = recipe.IngredientsText;
+                craftingButton.GetComponent<Button>().onClick.AddListener(()=>CraftingInventory.Craft(recipe));
+            }
+            Destroy(_craftingButton);
+        }
+    }
+}
